75a9eabb888db9cfbb210cdb8d988da0618d6fa1
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2020-03-30 20:42:56 +0700

    Install mask from releases/latest.assets endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,42 @@
 import * as core from '@actions/core'
+import {GitHub} from '@octokit/GitHub'
+import {downloadTool, extractZip, cacheDir} from '@actions/tool-cache'
+import {platform as getOS} from 'os'
+import * as path from 'path'
+import {randomBytes} from 'crypto'
+
+const MASK_REPO = {
+  owner: 'jakedeichert',
+  repo: 'mask'
+}
+const INSTALL_PATH = randomBytes(3).toString('hex')
 
 async function run(): Promise<void> {
   try {
     const commands = core.getInput('commands').split('\n')
     const parallel = core.getInput('parallel') === 'true'
+    const octokit = new GitHub({})
 
+    core.startGroup('install mask')
+    const {data: release} = await octokit.repos.getLatestRelease(MASK_REPO)
+    const assets = release.assets
+      .filter(({content_type: s}) => s.includes('octet-stream'))
+      .map(({name, browser_download_url: downloadURL}) => ({
+        downloadURL,
+        name: name.replace('.zip', ''),
+        platform: name.match(/darwin|linux|windows|openbsd|sun-solaris/)?.[0]
+      }))
+
+    const {downloadURL, name} = assets.filter(({platform: o}) => o === getOS())[0]
+    const zipFile = await downloadTool(downloadURL)
+    const extractPath = await extractZip(zipFile, INSTALL_PATH)
+    const cache = await cacheDir(
+      path.join(extractPath, name),
+      'mask',
+      release.tag_name
+    )
+    core.addPath(cache)
+    core.endGroup()
 
   } catch (error) {
     core.setFailed(error.message)
